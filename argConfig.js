274eb50c8d7e36578a803d5a2ad318d45d04cd16
girafferz <noiman2004+git@gmail.com>
2017-05-02 02:04:33 +0900

    Create argConfig.js

---
+++ argConfig.js
@@ -0,0 +1,5 @@
+exports.firstList = ["aka","ropp","shibu","koji","hanzo"]
+
+exports.secondList = ["miso","shio","sakana","ramen","wa","you","tyu","india","pasta","ie","cafe","pizza","niku","sweets"]
+
+exports.thirdList = ["miso","shio","sakana","ramen","wa","you","tyu","india","pasta","ie","cafe","pizza","niku","sweets"]
