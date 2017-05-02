64a5b5f7b21f7c04099bb629b5383a2c51449e58
girafferz <noiman2004+git@gmail.com>
2017-05-03 02:08:08 +0900

    Update argConfig.js

--- argConfig.js
+++ argConfig.js
@@ -1,5 +1,5 @@
 exports.firstList = ["aka","ropp","shibu","koji","hanzo"]
 
-exports.secondList = ["miso","shio","sakana","ramen","wa","you","tyu","india","pasta","ie","cafe","pizza","niku","sweets"]
+exports.secondList = ["miso","shio","sakana","ramen","wa","yo","tyu","india","pasta","ie","cafe","pizza","niku","sweets","jiro"]
 
-exports.thirdList = ["miso","shio","sakana","ramen","wa","you","tyu","india","pasta","ie","cafe","pizza","niku","sweets"]
+exports.thirdList = [1, 2, 3, 4, 5, "all"]
